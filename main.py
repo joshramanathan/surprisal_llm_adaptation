4ea98cc90a46698a7c0c926e95f2e5dc5b38cbcd
Josh Ramanathan <josh.ramanathan@gmail.com>
2025-04-28 19:20:16 -0500

    protect main.py from executing on import

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import surprisal_llm_adaptation
 
-runner = surprisal_llm_adaptation.ExperimentRunner(model_id="EleutherAI/pythia-1.4b")
-runner.run_experiment(num_proc=8, batch_size=9, block_size=128)
+def main():
+    # Initialize the experiment runner with the specified model ID
+    runner = surprisal_llm_adaptation.ExperimentRunner(model_id="EleutherAI/pythia-1.4b")
+    
+    # Run the experiment with specified parameters
+    runner.run_experiment(num_proc=8, batch_size=9, block_size=128)
+
+if __name__ == "__main__":
+    main()
